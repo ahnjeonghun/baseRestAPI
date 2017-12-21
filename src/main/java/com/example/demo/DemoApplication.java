fee57b8ba7d687d8b6148c8805b9f7ac769e82a1
aslan <an86baby@gmail.com>
2017-12-21 15:48:09 +0900

    Merge branch 'master' of https://github.com/ahnjeonghun/baseRestAPI
    # Conflicts:
    #	src/main/java/com/example/demo/domain/Member.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,29 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableTransactionManagement
 public class DemoApplication {
+
+    /*@Bean
+    RedisTemplate<String,String> rediStringRedisTemplate(){
+        RedisTemplate<String,String> redisTemplate = new RedisTemplate<String,String>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
